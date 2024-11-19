e6a30a6273e7b409ff7569ef88cac4d246f906c7
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2024-11-19 23:18:42 +0100

    Update RTC.ino

--- RTC.ino
+++ RTC.ino
@@ -1,22 +1,12 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#define ARD_PCF8583 0x68
+#define ADR_DS1302 0x68
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-struct Czas
-{
-    uint8_t s;
-    uint8_t m;
-    uint8_t h;   
-};
-
-
 class Zegarek
 {
-  public:
-
-  
+  public:  
   uint8_t print()
   {
     lcd.clear();
@@ -35,39 +25,34 @@ class Zegarek
   private:
   uint8_t Odczyt(uint8_t adr)
   {
-    Wire.beginTransmission(ARD_PCF8583);
+    Wire.beginTransmission(ADR_DS1302);
     Wire.write(adr);
     Wire.endTransmission();
     
-    Wire.requestFrom(ARD_PCF8583, 1);
-    //while (Wire.available()) {
-      return Wire.read();
+    Wire.requestFrom(ADR_DS1302, 1);
+    return Wire.read();
   }
-
 };
 
 Zegarek z;
 
-  
 void setup()
 {
   Wire.begin(); 
   Serial.begin(9600);
   lcd.init();
 /*
-    Wire.beginTransmission(ARD_PCF8583);
+    Wire.beginTransmission(ADR_DS1302);
     Wire.write(0);
     Wire.write(0x11);
     Wire.write(0x23);
     Wire.endTransmission();
-    Wire.write(0);
 */
 }
 
 void loop()
 {
   z.print();
-
   uint8_t c = z.Sekundy();   
   Serial.println(c,HEX);
   delay(1000);
